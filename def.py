d101b8b087053aafa0b171e188c8b6a3731b6120
Luda68 <pril3pskay@yandex.ru>
2024-05-24 18:51:56 +0500

    Add files via upload

---
+++ def.py
@@ -0,0 +1,9 @@
+def print_params():
+    print("Params")
+print_params()
+print_params()
+def print_params(a, b):
+    print("Params", a, b)
+    print("Params", a, b)
+print_params("раз","два")
+print_params("три","четыри")
